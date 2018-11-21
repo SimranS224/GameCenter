bf08732ff3d7eb0da4453e68bd1589b5a5f92845
Ryan Foster <ryk.foster@mail.utoronto.ca>
2018-11-20 20:32:44 -0500

    Fixed swipeView again; Needed to reclone

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoard/swipeTest.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoard/swipeTest.java
@@ -3,11 +3,14 @@ package fall2018.csc2017.GameCentre.ScoreBoard;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-
 import fall2018.csc2017.GameCentre.R;
 
 
+
+/**
+ * This is the actual activity that manages the swipeView; Nothing needs to be
+ * changed in here.
+ */
 public class swipeTest extends FragmentActivity {
     static final int num_list = 6;
 
@@ -19,14 +22,11 @@ public class swipeTest extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Log.e("Test", "1");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.swipeview_test);
-        Log.e("Test", "2");
         mAdapter = new swipeViewAdapter(getSupportFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager_test);
         mPager.setAdapter(mAdapter);
-        Log.e("Test", "3");
 
 
 
