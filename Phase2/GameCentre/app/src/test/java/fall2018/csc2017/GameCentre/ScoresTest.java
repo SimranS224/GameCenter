81abc01b077b89337ced3dc17890a7beec8eee1e
gosen <jheff.chen@mail.utoronto.ca>
2018-11-29 15:22:17 -0500

    Testcases for Scores, User scores, User scores not done yet

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoresTest.java
@@ -0,0 +1,70 @@
+package fall2018.csc2017.GameCentre;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ScoresTest {
+
+    Scores score;
+
+    @Before
+    public void setUp() {
+        score = new Scores("Frank","1");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetScore() {
+        setUp();
+        String scoretest;
+        scoretest = score.getScore();
+        assertEquals("1", scoretest);
+    }
+
+    @Test
+    public void testGetName() {
+        setUp();
+        String nametest;
+        nametest = score.getName();
+        assertEquals("Frank", nametest);
+    }
+
+    @Test
+    public void getIntValue() {
+        setUp();
+        int scoretest;
+        scoretest = score.getIntValue();
+        assertEquals(1, scoretest);
+    }
+
+    @Test
+    public void setName() {
+        Scores newscore = new Scores();
+        newscore.setName("Frank");
+        assertEquals("Frank", newscore.getName());
+    }
+
+    @Test
+    public void setScore() {
+        Scores newscore = new Scores();
+        newscore.setScore("1");
+        assertEquals("1", newscore.getScore());
+    }
+
+    @Test
+    public void testCompareTo() {
+        Scores compare = new Scores("Me", "0");
+        assertEquals(1, score.compareTo(compare));
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Frank, 1", score.toString());
+    }
+}
---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/UserScoresTest.java
@@ -0,0 +1,80 @@
+package fall2018.csc2017.GameCentre;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import fall2018.csc2017.GameCentre.ScoreBoard.ScoreBoardController.Scores;
+import fall2018.csc2017.GameCentre.ScoreBoard.ScoreBoardController.UserScores;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserScoresTest {
+    public ArrayList<Scores> array;
+    UserScores userscores;
+    Scores score = new Scores("frank", "0");
+    @Before
+    public void setUp() {
+        userscores = new UserScores();
+        ArrayList<Scores> array = new ArrayList<>();
+        array.add(score);
+    }
+
+    @Test
+    public void testSetArray() {
+        setUp();
+        userscores.setArray(array);
+        //  assertEquals(, userscores.getArray());
+    }
+
+    @Test
+    public void getArray() {
+
+    }
+
+    @Test
+    public void testContains() {
+        setUp();
+        assertEquals(true, userscores.contains("frank"));
+        assertEquals(false, userscores.contains("fr"));
+    }
+
+    @Test
+    public void size() {
+        setUp();
+        assertEquals(1, userscores.array.size());
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void add() {
+
+    }
+
+    @Test
+    public void testIsEmpty() {
+        setUp();
+        assertEquals(false, userscores.isEmpty());
+        //TODO add a false scenario
+
+    }
+
+    @Test
+    public void addLowerScore() {
+    }
+
+    @Test
+    public void testEmpty() {
+        setUp();
+        userscores.empty();
+        assertEquals(true, userscores.size() == 0);
+    }
+
+    @Test
+    public void getUser() {
+    }
+}
