8c4b69ae841c303bcf5620908dac1d50223cd2de
Simran <snike2610@gmail.com>
2018-11-29 21:47:26 -0500

    leaderboard test

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTiles/BoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTiles/BoardManager.java
@@ -11,7 +11,7 @@ import fall2018.csc2017.GameCentre.Manager;
 /**
  * Manage a board, including swapping tiles, checking for a win, and managing taps.
  */
-class BoardManager implements Serializable, Manager {
+public class BoardManager implements Serializable, Manager {
 
     /**
      * score
@@ -29,7 +29,7 @@ class BoardManager implements Serializable, Manager {
      *
      * @param board the board
      */
-    BoardManager(Board board) {
+    public BoardManager(Board board) {
         this.board = board;
         this.score = 0L;
         this.stack = new MoveStack();
