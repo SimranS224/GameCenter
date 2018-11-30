c6ef3c76d2fb2dda3e653fc59f6644cad5115b2f
Simran <snike2610@gmail.com>
2018-11-30 00:44:49 -0500

    leaderboardreadertest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoreBoard/ScoreBoardController/LeaderBoardReaderTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoreBoard/ScoreBoardController/LeaderBoardReaderTest.java
@@ -7,44 +7,74 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
+/**
+ * The LeaderBoardReader Test Class
+ */
 public class LeaderBoardReaderTest {
 
+    /**
+     * A LeaderBoardReader for testing.
+     */
     private LeaderBoardReader testOne;
-    private ArrayList<UserScores> listTemp;
+    /**
+     * An temporary arraylist of UserScores (i.e highscores)
+     */
+    private ArrayList<Scores> listTemp;
 
+    /**
+     * Creates a LeaderBoardReader, and temporary list.
+     */
     @Before
     public void setUp() {
-        UserScores newOne = new UserScores();
+        testOne = new LeaderBoardReader();
         Scores sample = new Scores("Jeff", "10");
-        newOne.add(sample);
-        UserScores anotherOne = new UserScores();
-        Scores anotherSampleOne = new Scores("Bob", "Dlyan");
-        anotherOne.add(anotherSampleOne);
-        listTemp.add(anotherOne);
-        listTemp.add(newOne);
+        Scores anotherSampleOne = new Scores("Bob", "10");
+        listTemp = new ArrayList<>();
+        listTemp.add(sample);
+        listTemp.add(anotherSampleOne);
 
     }
 
     /**
-     * Tests to make sure the
+     * Tests to make sure the content is updated.
      */
     @Test
     public void addAllToContents() {
         setUp();
         testOne.addAllToContents(listTemp);
-        assertEquals(2, testOne.getSize());
+        System.out.println(testOne.getContents());
+        assertEquals(1, testOne.getSize());
 
     }
 
+    /**
+     * Test to make sure the contents of LeaderBoardReader are returned as desired.
+     */
     @Test
     public void getContents() {
+        setUp();
+        testOne.addAllToContents(listTemp);
+        assertNotNull(testOne.getContents());
+
     }
 
+    /**
+     * Test to make sure the size of LeaderBoardReader is correct.
+     */
     @Test
     public void getSize() {
+        setUp();
+        testOne.addAllToContents(listTemp);
+        assertEquals(1, testOne.getSize());
     }
 
+    /**
+     * Tests to make sure UserScore object can be added to the contents of LeaderBoardReader
+     */
     @Test
     public void add() {
+        setUp();
+        testOne.addAllToContents(listTemp);
+        assertEquals(1, testOne.getSize());
     }
 }
