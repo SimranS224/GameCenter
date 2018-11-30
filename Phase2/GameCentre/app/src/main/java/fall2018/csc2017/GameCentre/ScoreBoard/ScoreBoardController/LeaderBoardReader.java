c9e07eb157f3384af8fa8ef7d71089619c01e89b
Simran <snike2610@gmail.com>
2018-11-29 23:41:17 -0500

    refactoring leaderboard

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoard/ScoreBoardController/LeaderBoardReader.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ScoreBoard/ScoreBoardController/LeaderBoardReader.java
@@ -3,14 +3,25 @@ package fall2018.csc2017.GameCentre.ScoreBoard.ScoreBoardController;
 import java.util.ArrayList;
 
 
+/**
+ * A LeaderBoardReaderClass
+ */
 public class LeaderBoardReader {
 
+    /**
+     * The contents of LeaderBoardReader.
+     */
     private ArrayList<UserScores> contents;
 
     public LeaderBoardReader() {
         this.contents = new ArrayList<>();
     }
 
+    /**
+     * Adds a list of hashed highscores for all games from firebase to the contents of LeaderBoardReader.
+     *
+     * @param list the hashed lists of highscores
+     */
     public void addAllToContents(ArrayList list) {
         UserScores curList = new UserScores();
         for (int i = 0; i < list.size(); i++) {
@@ -20,15 +31,30 @@ public class LeaderBoardReader {
 
     }
 
+    /**
+     * Return the list of all highscores in a non-hashed format.
+     *
+     * @return the contents of LeaderBoardReader
+     */
     public ArrayList<UserScores> getContents() {
         return this.contents;
     }
 
+    /**
+     * Return the number of elements in contents.
+     *
+     * @return the size of the contents of LeaderBoardReader
+     */
     public int getSize() {
         return contents.size();
     }
 
-    public void add(UserScores emptyOne) {
-        this.contents.add(emptyOne);
+    /**
+     * Adds a list of highscores to contents.
+     *
+     * @param newOne a list of User Scores to add.
+     */
+    public void add(UserScores newOne) {
+        this.contents.add(newOne);
     }
 }
